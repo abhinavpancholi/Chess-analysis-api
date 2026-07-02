ab9a30d970245f61710b566857d1be606030aebd
abhinav <abhinavpancholi30@gmail.com>
2026-07-03 01:43:02 +0530

    Prep for Railway deployment

--- app/main.py
+++ app/main.py
@@ -36,10 +36,7 @@ app = FastAPI(title="Chess Analysis API", lifespan=lifespan)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://localhost:5173",
-        "http://localhost:3000",
-    ],
+    allow_origins=settings.allowed_origins_list,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
